89a14fc1a66d266253a6bf340514e0a6f4a64871
adi-uchiha <adi.cs.night@gmail.com>
2023-08-13 19:14:46 +0530

    add prisma posgres and api

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { ModalProvider } from '@/providers/modal-provider'
+import prismadb from '@/lib/prismadb'
+import { ToastProvider } from '@/providers/toast-provider'
 
 const inter = Inter({ subsets: ['latin'],})
 
@@ -15,11 +17,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+  const store = prismadb.store
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
           <ModalProvider />
+          <ToastProvider />
           {children}
           </body>
       </html>
